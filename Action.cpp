0cf2381d1ef488f6e05bf23b968f7752d8982ea5
FireFox <115025745+20FireFox02@users.noreply.github.com>
2023-12-21 11:06:14 +0300

    Delete Action.cpp

--- Action.cpp
+++
@@ -1,8 +0,0 @@
-#include "Action.hpp"
-
-void Action::Set_New_State(std::string newstate) { this->new_state = newstate; }
-void Action::Set_New_Word(std::string newword) { this->new_word = newword; }
-void Action::Set_Move(char move) { this->move = move; }
-std::string Action::Get_New_State() const { return new_state; }
-std::string Action::Get_New_Word() const { return new_word; }
-char Action::Get_Move() const { return move; }
